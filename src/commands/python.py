7add2f5c61a6518185fa4262e10035cf4e8af4e0
stackotter <stackotter@stackotter.dev>
2022-08-11 21:55:01 +1000

    Put python execution output in reply instead of standalone message

--- src/commands/python.py
+++ src/commands/python.py
@@ -22,8 +22,11 @@ async def eval_python(event: hikari.MessageCreateEvent):
 
     code_block = extract_code_block(event.message.content)
 
-    if not code_block.lang in ["py", "python"]:
-        await event.message.respond("Only python is supported")
+    if code_block.lang == None:
+        await event.message.respond("No language specified, assuming python", reply=True)
+        code_block.lang = "python"
+    elif not code_block.lang in ["py", "python"]:
+        await event.message.respond("Only python is supported", reply=True)
         return
 
     # Create execution directory containing script
@@ -51,19 +54,19 @@ async def eval_python(event: hikari.MessageCreateEvent):
         response = f"**Process returned non-zero exit status {e.returncode}**"
         if e.output != None and len(e.output.strip()) != 0:
             response += f"\n```\n{e.output.decode('utf8').strip()}\n```"
-        await event.message.respond(response)
+        await event.message.respond(response, reply=True)
         return
     except Exception as e:
-        await event.message.respond(f"**Error while running code**: `{e}`")
+        await event.message.respond(f"**Error while running code**: `{e}`", reply=True)
         return
     output = output.strip()
 
     shutil.rmtree(temp_dir)
 
     if len(output) == 0:
-        await event.message.respond("No output")
+        await event.message.respond("No output", reply=True)
     else:
-        await event.message.respond(f"```\n{output}\n```")
+        await event.message.respond(f"```\n{output}\n```", reply=True)
 
 def load(bot):
     bot.add_plugin(plugin)
