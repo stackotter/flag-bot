d936fffffa8ad2b9d015ce563883127d483feae8
stackotter <stackotter@stackotter.dev>
2022-08-12 17:15:02 +1000

    Ensure that execution environment gets cleaned up even if user-code throws an error

--- src/commands/code_runner.py
+++ src/commands/code_runner.py
@@ -2,12 +2,13 @@ import subprocess
 import lightbulb
 import resource
 import tempfile
+import asyncio
 import shutil
 import hikari
 import pwd
 import sys
 
-from contextlib import redirect_stdout, redirect_stderr
+from contextlib import redirect_stdout, redirect_stderr, ExitStack
 from io import StringIO
 
 from ..message import extract_code_block
@@ -21,8 +22,8 @@ async def eval_python(event: hikari.MessageCreateEvent):
     if mentioned_member_ids != [plugin.bot.get_me().id]:
         return
 
+    # Parse message's code block if present
     code_block = extract_code_block(event.message.content)
-
     if code_block.lang == None:
         await event.message.respond("No language specified, assuming python", reply=True)
         code_block.lang = "python"
@@ -30,6 +31,7 @@ async def eval_python(event: hikari.MessageCreateEvent):
         await event.message.respond("Only python is supported", reply=True)
         return
 
+    # Figure out how to run the code
     extension = "sh" if code_block.lang == "sh" else "py"
     interpreter = "/bin/sh" if code_block.lang == "sh" else sys.executable
 
@@ -39,46 +41,52 @@ async def eval_python(event: hikari.MessageCreateEvent):
     with open(temp_file, "w") as f:
         f.write(code_block.code)
 
-    # Allow sandbox user access to execution directory and script
+    # Give sandbox user access to execution directory
     entry = pwd.getpwnam(sandbox_user)
     sandbox_uid = entry.pw_uid
     sandbox_gid = entry.pw_gid
     shutil.chown(temp_dir, sandbox_uid, sandbox_gid)
     shutil.chown(temp_file, sandbox_uid, sandbox_gid)
 
-    try:
-        output = subprocess.check_output(
-            [interpreter, temp_file],
-            stderr=subprocess.STDOUT,
-            cwd=temp_dir,
-            timeout=2,
-            user=sandbox_uid,
-            group=sandbox_gid
-        ).decode("utf8")
-    except subprocess.CalledProcessError as e:
-        response = f"**Process returned non-zero exit status {e.returncode}**"
-        if e.output != None and len(e.output.strip()) != 0:
-            response += f"\n```\n{e.output.decode('utf8').strip()}\n```"
-        await event.message.respond(response, reply=True)
-        return
-    except Exception as e:
-        await event.message.respond(f"**Error while running code**: `{e}`", reply=True)
-        return
+    # Clean up execution environment
+    def cleanup():
+        try:
+            entry = pwd.getpwnam(subprocess.check_output(["whoami"]).decode("utf8").strip())
+            # Avoid killing important processes if sandbox user is current user (which would be a
+            # dev env)
+            if sandbox_uid != entry.pw_uid:
+                subprocess.run(["pkill", "-u", str(sandbox_uid)])
+        except Exception as e:
+            asyncio.run_coroutine_threadsafe(event.message.respond(f"**Failed to kill all processes created by snippet**: `{e}`", reply=True), asyncio.get_event_loop())
+            return
 
+        shutil.rmtree(temp_dir)
+
+    # Run the code and clean up execution environment no matter what happens
+    output = None
+    with ExitStack() as stack:
+        stack.callback(cleanup)
+        try:
+            output = subprocess.check_output(
+                [interpreter, temp_file],
+                stderr=subprocess.STDOUT,
+                cwd=temp_dir,
+                timeout=2,
+                user=sandbox_uid,
+                group=sandbox_gid
+            ).decode("utf8")
+        except subprocess.CalledProcessError as e:
+            response = f"**Process returned non-zero exit status {e.returncode}**"
+            if e.output != None and len(e.output.strip()) != 0:
+                response += f"\n```\n{e.output.decode('utf8').strip()}\n```"
+            await event.message.respond(response, reply=True)
+            return
+        except Exception as e:
+            await event.message.respond(f"**Error while running code**: `{e}`", reply=True)
+            return
+
+    # Handle code output
     output = output.strip()
-
-    # Kill all subprocesses that the code might have created, unless the sandbox user is the current
-    # user
-    try:
-        entry = pwd.getpwnam(subprocess.check_output(["whoami"]).decode("utf8").strip())
-        if sandbox_uid != entry.pw_uid:
-            subprocess.run(["pkill", "-u", str(sandbox_uid)])
-    except Exception as e:
-        await event.message.respond(f"**Failed to kill all processes created by snippet**: `{e}`", reply=True)
-        return
-
-    shutil.rmtree(temp_dir)
-
     if len(output) == 0:
         await event.message.respond("No output", reply=True)
     else:
